44b6bccbb740593daa374213b105280d6527b31e
Campbell Barton <ideasman42@gmail.com>
2016-12-15 21:56:24 +1100

    Add license and keywords

--- inkpot-theme.el
+++ inkpot-theme.el
@@ -4,6 +4,22 @@
 ;;         Campbell Barton <ideasman42@gmail.com>
 ;; URL: http://github.com/ideasman42/emacs24-inkpot.git
 ;; Version: 0.1
+;; Keywords: color, theme
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;; This file is not part of Emacs.
 
 ;;; Commentary:
 
